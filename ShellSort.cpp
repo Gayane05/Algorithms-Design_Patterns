20aad03f868d95cc25f60d93a66240680b63f437
Gayane Grigoryan <44817376+Gayane05@users.noreply.github.com>
2021-02-01 17:01:42 +0400

    ShellSort.cpp
    ShellSort

---
+++ ShellSort.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+template <typename T>
+void ShellSort(std::vector<T> arr)
+{
+	int h = 1;
+	while (h < arr.size() / 3)
+	{
+		h = h * 3 + 1;
+	}
+
+	while(h >= 1)
+	{
+		for (int i = h; i < arr.size(); ++i)
+		{
+			for (int j = i; j >= h && arr[j] - arr[j - h]; j -= h)
+			{
+				std::swap(arr[j], arr[j-h]);
+			}
+		}
+		h /= 3;
+	}
+}
+
+
+int main()
+{
+	std::vector<char> example{ 'S','O','R','T','E','X','A','M','P','L','E' };
+	ShellSort(example);
+
+
+	return 0;
+}
+
+
+/*
+int size = arr.size();
+	int h = 1;
+	while (h < size / 3)
+	{
+		h = 3 * h + 1;
+	}
+
+	while (h >= 1)
+	{
+		for (int i = h; i < size; ++i)
+		{
+			for (int j = i; j >= h && (arr[j] < arr[j - h]); j -= h)
+			{
+				std::swap(arr[j], arr[j - h]);
+			}
+		}
+		h = h / 3;
+	}
+
+*/
